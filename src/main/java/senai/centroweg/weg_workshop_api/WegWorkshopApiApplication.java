b80e8edba64abe820f09e8dd10b1af047af11573
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-26 15:17:40 -0300

    Initial Commit

---
+++ src/main/java/senai/centroweg/weg_workshop_api/WegWorkshopApiApplication.java
@@ -0,0 +1,13 @@
+package senai.centroweg.weg_workshop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegWorkshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WegWorkshopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/senai/centroweg/weg_workshop_api/WegWorkshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package senai.centroweg.weg_workshop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WegWorkshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
