dfa57acccfa4e3209394af1efbe41cff74c61bb8
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-26 20:01:35 -0300

    feat: implement User service with mappers

--- src/main/java/senai/centroweg/weg_workshop_api/application/service/UserService.java
+++ src/main/java/senai/centroweg/weg_workshop_api/application/service/UserService.java
@@ -2,33 +2,46 @@ package senai.centroweg.weg_workshop_api.application.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import senai.centroweg.weg_workshop_api.domain.enums.UserType;
+import senai.centroweg.weg_workshop_api.application.dto.request.UserRequestDTO;
+import senai.centroweg.weg_workshop_api.application.dto.response.UserResponseDTO;
+import senai.centroweg.weg_workshop_api.application.mapper.UserMapper;
 import senai.centroweg.weg_workshop_api.domain.model.User;
 import senai.centroweg.weg_workshop_api.domain.ports.UserRepositoryPort;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
     private final UserRepositoryPort userRepositoryPort;
+    private final UserMapper userMapper;
 
     @Autowired
-    public UserService(UserRepositoryPort userRepositoryPort) {
+    public UserService(UserRepositoryPort userRepositoryPort, UserMapper userMapper) {
         this.userRepositoryPort = userRepositoryPort;
+        this.userMapper = userMapper;
     }
 
-    public User createUser(String name, UserType userType) {
-        User user = new User(name, userType);
-        return userRepositoryPort.save(user);
+    public UserResponseDTO createUser(UserRequestDTO request) {
+
+        User user = userMapper.toEntity(request);
+        User savedUser = userRepositoryPort.save(user);
+
+        return userMapper.toResponseDTO(savedUser);
     }
 
-    public List<User> listAll() {
-        return userRepositoryPort.listAll();
+    public List<UserResponseDTO> listAll() {
+
+        return userRepositoryPort.listAll().stream()
+                .map(userMapper::toResponseDTO)
+                .collect(Collectors.toList());
     }
 
-    public Optional<User> findById(Integer id) {
-        return userRepositoryPort.findById(id);
+    public UserResponseDTO findById(Integer id) {
+
+        return userRepositoryPort.findById(id)
+                .map(userMapper::toResponseDTO)
+                .orElseThrow(() -> new RuntimeException("User not found"));
     }
 }
